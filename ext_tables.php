90f6f4b5cf2d10b28b90f54b835283d76d9608b8
Dirk Wenzel <wenzel@cps-it.de>
2023-05-10 11:14:02 +0200

    inclusion of JavaScript and TypoScript edited

---
+++ ext_tables.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3calendar', 'Configuration/TypoScript', 'Calendar');
